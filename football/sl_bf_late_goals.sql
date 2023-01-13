9d71c4fb5c022df3190518fb5cbfb0df728e7d4e
AngusMcKay <angusjamesmckay@gmail.com>
2023-01-13 11:48:50 +0000

    bit more dev

--- football/sl_bf_late_goals.sql
+++ football/sl_bf_late_goals.sql
@@ -27,12 +27,15 @@ select count(1) from testing_matches_pre_prediction_times;
 
 select count(1) from testing_matches_df;
 
-select count(1) from testing_model_data order by datetime_utc desc;
+select count(1) from testing_model_data;
 
 select count(1) from testing_viable_matches;
 
+select count(1) from testing3_viable_matches_with_unmatched_goals;
 
 
-select * from testing3_model_data_with_preds;
+select total_available, count(1) from testing3_model_data_with_preds group by 1 order by 2 desc limit 100;
+
+select * from testing3_model_data_with_preds where total_available = 7351.86;
 
 select min(datetime_utc) from testing_viable_matches;
