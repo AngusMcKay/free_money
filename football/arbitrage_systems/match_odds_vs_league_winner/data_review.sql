9e9046be2c3ab8442fd7578342e0e150cdd4433e
AngusMcKay <angusjamesmckay@gmail.com>
2025-01-12 10:29:05 +0000

    minor additions

--- football/arbitrage_systems/match_odds_vs_league_winner/data_review.sql
+++ football/arbitrage_systems/match_odds_vs_league_winner/data_review.sql
@@ -4,7 +4,7 @@ SELECT * FROM league_odds ORDER BY datetime_extracted LIMIT 10;
 
 select * FROM match_odds LIMIT 10;
 
-
-
-;
+SELECT * FROM league_odds
+WHERE runner_name LIKE '%Man%Utd%' AND market_type = 'WINNER'
+ORDER BY datetime_extracted DESC LIMIT 10;
 
